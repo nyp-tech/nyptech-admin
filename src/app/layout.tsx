38fe0a7a2a55ae28de87f517bebf650a341bc670
Dennise Catolos <dentolos19@gmail.com>
2024-07-13 09:49:51 +0800

    Updated biome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,37 +5,37 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "NYP Technopreneurship Club",
+  title: "NYP Technopreneurship Club",
 };
 
 export default function Layout(
-	props: Readonly<{
-		children: React.ReactNode;
-	}>,
+  props: Readonly<{
+    children: React.ReactNode;
+  }>,
 ) {
-	return (
-		<html lang="en">
-			<head>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-			</head>
-			<body className={inter.className}>{props.children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className={inter.className}>{props.children}</body>
+    </html>
+  );
 }
