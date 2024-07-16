1c92f81042431eab67eaeef3aa83d283a4c054d7
Dennise Catolos <dentolos19@gmail.com>
2024-07-16 16:11:25 +0800

    Updated environment variables

--- src/environment.ts
+++ src/environment.ts
@@ -2,4 +2,4 @@ export const AdminKey = process.env.ADMIN_KEY as string;
 export const EncryptionKey = process.env.ENCRYPTION_KEY as string;
 
 // Go Microservice
-export const MsGo_Key = process.env.MS_GO_KEY as string;
+export const MsGo_Key = process.env.MICROSERVICE_GO_KEY as string;
